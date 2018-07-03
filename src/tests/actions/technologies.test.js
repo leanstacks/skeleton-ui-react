5e569b85201bad0ee0d3be81da7fe04a558d054e
Matt Warman <warmanmr@gmail.com>
2018-07-03 14:53:08 -0400

    Asynchronous actions tests. Axios mocks. LocalStorage mocks.

--- src/tests/actions/technologies.test.js
+++ src/tests/actions/technologies.test.js
@@ -1,6 +1,19 @@
-import { setTechnologies } from '../../actions/technologies';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import mockAxios from 'axios';
+
+import { 
+  setTechnologies,
+  startSetTechnologies
+} from '../../actions/technologies';
 
 import technologies from '../fixtures/technologies';
+
+const defaultState = {
+  technologies: []
+};
+const createMockStore = configureMockStore([thunk]);
 
 test('should setup setTechnologies action object', () => {
   const action = setTechnologies(technologies);
@@ -8,5 +21,24 @@ test('should setup setTechnologies action object', () => {
   expect(action).toEqual({
     type: 'SET_TECHNOLOGIES',
     technologies
+  });
+});
+
+test('should fetch technologies from API', (done) => {
+  const store = createMockStore(defaultState);
+  mockAxios.request.mockImplementationOnce(() => 
+    Promise.resolve({
+      data: technologies
+    })
+  );
+
+  store.dispatch(startSetTechnologies()).then(() => {
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type: 'SET_TECHNOLOGIES',
+      technologies
+    });
+
+    done();
   });
 });
