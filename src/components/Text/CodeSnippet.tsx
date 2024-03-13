021f7ce6f2a706564eef65b0fb895bf2b915dae9
Matthew Warman <warmanmr@gmail.com>
2024-03-13 16:43:44 -0400

    code snippet

--- src/components/Text/CodeSnippet.tsx
+++ src/components/Text/CodeSnippet.tsx
@@ -1,5 +1,6 @@
 import { SandpackProvider, SandpackLayout, SandpackCodeEditor } from '@codesandbox/sandpack-react';
 import { PropsWithClassName, PropsWithTestId } from '@leanstacks/react-common';
+import { useSettings } from 'providers/SettingsProvider';
 
 interface CodeSnippetProps extends PropsWithClassName, PropsWithTestId {
   code: string;
@@ -10,13 +11,14 @@ const CodeSnippet = ({
   code,
   testId = 'code-snippet',
 }: CodeSnippetProps): JSX.Element => {
+  const { theme } = useSettings();
   const files = {
     '/App.js': code,
   };
 
   return (
     <div className={className} data-testid={testId}>
-      <SandpackProvider template="react" theme="dark" files={files}>
+      <SandpackProvider template="react" theme={theme} files={files}>
         <SandpackLayout>
           <SandpackCodeEditor
             className="!h-fit rounded-lg"
