882012eea75958fbd08787e21033f68736cd4564
Matthew Warman <warmanmr@gmail.com>
2024-03-14 14:51:41 -0400

    tests

---
+++ src/api/__tests__/useGetUserTokens.test.ts
@@ -0,0 +1,16 @@
+import { useGetUserTokens } from 'api/useGetUserTokens';
+import { renderHook, waitFor } from 'test/test-utils';
+
+describe('useGetTokens', () => {
+  it('should get user tokens', async () => {
+    // ARRANGE
+    const { result } = renderHook(() => useGetUserTokens());
+    await waitFor(() => expect(result.current.isSuccess).toBe(true));
+
+    // ASSERT
+    expect(result.current.isSuccess).toBe(true);
+    expect(result.current.isError).toBe(false);
+    expect(result.current.data).toBeDefined();
+    expect(result.current.data?.id_token).toBe('id-token');
+  });
+});
