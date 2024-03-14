6b1cb21eec6b37bb786be7e4a9e5e4cfe38bf8a2
Matthew Warman <warmanmr@gmail.com>
2024-03-14 07:52:01 -0400

    tests

---
+++ src/components/Page/__tests__/Page.test.tsx
@@ -0,0 +1,44 @@
+import { render, screen } from 'test/test-utils';
+import Page from '../Page';
+
+describe('Page', () => {
+  it('should render successfully', async () => {
+    // ARRANGE
+    render(
+      <Page>
+        <div data-testid="content" />
+      </Page>,
+    );
+    await screen.findByTestId('page');
+
+    // ASSERT
+    expect(screen.getByTestId('page')).toBeDefined();
+    expect(screen.getByTestId('content')).toBeDefined();
+  });
+
+  it('should use custom testId', async () => {
+    // ARRANGE
+    render(
+      <Page testId="custom-testId">
+        <div data-testid="content" />
+      </Page>,
+    );
+    await screen.findByTestId('custom-testId');
+
+    // ASSERT
+    expect(screen.getByTestId('custom-testId')).toBeDefined();
+  });
+
+  it('should use custom className', async () => {
+    // ARRANGE
+    render(
+      <Page className="custom-className">
+        <div data-testid="content" />
+      </Page>,
+    );
+    await screen.findByTestId('page');
+
+    // ASSERT
+    expect(screen.getByTestId('page').classList).toContain('custom-className');
+  });
+});
