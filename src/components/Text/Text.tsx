f0aefc286559725f6952cef7209fa6cdc35d925b
Matthew Warman <warmanmr@gmail.com>
2024-03-14 08:38:53 -0400

    docs

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -2,12 +2,28 @@ import { PropsWithClassName, PropsWithTestId } from '@leanstacks/react-common';
 import classNames from 'classnames';
 import { PropsWithChildren } from 'react';
 
+/**
+ * The variations of `Text` components.
+ */
 export type TextVariant = 'heading1' | 'heading2' | 'heading3';
 
+/**
+ * Properties for the `Text` React component.
+ * @param {TextVariant} [variant] - Optional. The variant. Default: `none`
+ * @see {@link PropsWithChildren}
+ * @see {@link PropsWithClassName}
+ * @see {@link PropsWithTestId}
+ */
 interface TextProps extends PropsWithChildren, PropsWithClassName, PropsWithTestId {
   variant?: TextVariant;
 }
 
+/**
+ * The `Text` component renders a block of text. The text is styled to match
+ * the supplied `variant`.
+ * @param {TextProps} props - Component properties.
+ * @returns {JSX.Element} JSX
+ */
 const Text = ({ children, className, testId = 'text', variant }: TextProps): JSX.Element => {
   const getVariantClasses = (variant?: TextVariant): string => {
     switch (variant) {
