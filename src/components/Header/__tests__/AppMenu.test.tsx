3320eea947a41c5f0ccde08adb1710e5dd0db111
Matthew Warman <warmanmr@gmail.com>
2024-03-17 07:06:38 -0400

    tests

--- src/components/Header/__tests__/AppMenu.test.tsx
+++ src/components/Header/__tests__/AppMenu.test.tsx
@@ -22,51 +22,55 @@ describe('AppMenu', () => {
   });
 
   it('should render successfully', async () => {
+    // ARRANGE
     render(<AppMenu />);
-
     await screen.findByTestId('menu-app');
 
+    // ASSERT
     expect(screen.getByTestId('menu-app')).toBeDefined();
   });
 
   it('should use custom testId', async () => {
+    // ARRANGE
     render(<AppMenu testId="custom-testid" />);
-
     await screen.findByTestId('custom-testid');
 
+    // ASSERT
     expect(screen.getByTestId('custom-testid')).toBeDefined();
   });
 
   it('should use custom className', async () => {
+    // ARRANGE
     render(<AppMenu className="custom-class" />);
-
     await screen.findByTestId('menu-app');
 
+    // ASSERT
     expect(screen.getByTestId('menu-app').classList).toContain('custom-class');
   });
 
   it('should render authenticated content', async () => {
+    // ARRANGE
     render(<AppMenu />);
-
     await screen.findByTestId('menu-app');
 
+    // ASSERT
     expect(screen.getByTestId('menu-app')).toBeDefined();
     expect(screen.getByText(userFixture1.name)).toBeDefined();
     expect(screen.getByText('Sign Out')).toBeDefined();
   });
 
   it('should render unauthenticated content', async () => {
+    // ARRANGE
     useAuthContextSpy.mockReturnValue({
       isAuthenticated: false,
     });
     useGetUserSpy.mockReturnValue({
       data: undefined,
     } as unknown as UseQueryResult<UseGetUser.User>);
-
     render(<AppMenu />);
-
     await screen.findByTestId('menu-app');
 
+    // ASSERT
     expect(screen.getByTestId('menu-app')).toBeDefined();
     expect(screen.getByAltText('Logo')).toBeDefined();
     expect(screen.getByText('Sign In')).toBeDefined();
--- src/components/Icon/__tests__/Avatar.test.tsx
+++ src/components/Icon/__tests__/Avatar.test.tsx
@@ -4,43 +4,48 @@ import Avatar from '../Avatar';
 
 describe('Avatar', () => {
   it('should render successfully', async () => {
+    // ARRANGE
     render(<Avatar value="test" />);
-
     await screen.findByTestId('icon-avatar');
 
+    // ASSERT
     expect(screen.getByTestId('icon-avatar')).toBeDefined();
   });
 
   it('should render custom testId', async () => {
+    // ARRANGE
     render(<Avatar value="test" testId="custom-testid" />);
-
     await screen.findByTestId('custom-testid');
 
+    // ASSERT
     expect(screen.getByTestId('custom-testid')).toBeDefined();
   });
 
   it('should render custom className', async () => {
+    // ARRANGE
     render(<Avatar value="test" className="custom-classname" />);
-
     await screen.findByTestId('icon-avatar');
 
+    // ASSERT
     expect(screen.getByTestId('icon-avatar').classList).toContain('custom-classname');
   });
 
   it('should render picture', async () => {
+    // ARRANGE
     const imageSource = 'https://placehold.co/32x32.png';
     render(<Avatar value="test" picture={imageSource} />);
-
     await screen.findByTestId('icon-avatar');
 
+    // ASSERT
     expect(screen.getByTestId('icon-avatar').getAttribute('src')).toBe(imageSource);
   });
 
   it('should render default initial when no value provided', async () => {
+    // ARRANGE
     render(<Avatar value=" " />);
-
     await screen.findByTestId('icon-avatar');
 
+    // ASSERT
     expect(screen.getByTestId('icon-avatar')).toHaveTextContent('?');
   });
 });
