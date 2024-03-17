3385805e7dd022a9d34e2d4e6289d9bd5379b85c
Matthew Warman <warmanmr@gmail.com>
2024-03-17 07:29:50 -0400

    tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,10 +4,11 @@ import App from './App';
 
 describe('App', () => {
   it('should render successfully', async () => {
+    // ARRANGE
     render(<App />);
-
     await screen.findByTestId('app');
 
+    // ASSERT
     expect(screen.getByTestId('app')).toBeDefined();
   });
 });
