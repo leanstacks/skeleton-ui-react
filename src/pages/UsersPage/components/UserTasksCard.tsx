efced3dea6b1d8ce1e67aa735bf7b3aa9c8dd23d
Matt Warman <mwarman@users.noreply.github.com>
2024-05-27 17:37:07 -0400

    #51 User Tasks Dashboard Card (#52)
    #51 usertaskscard component

---
+++ src/pages/UsersPage/components/UserTasksCard.tsx
@@ -0,0 +1,75 @@
+import { useMemo } from 'react';
+import { useNavigate } from 'react-router-dom';
+import filter from 'lodash/filter';
+import classNames from 'classnames';
+
+import { useGetUserTasks } from '../api/useGetUserTasks';
+import Card, { CardProps } from 'components/Card/Card';
+import LoaderSkeleton from 'components/Loader/LoaderSkeleton';
+
+/**
+ * Properties for the `UserTasksCard` React component.
+ * @param {number} userId - A User identifier.
+ * @see {@link CardProps}
+ */
+interface UserTasksCardProps extends CardProps {
+  userId: number;
+}
+
+/**
+ * The `UserTasksCard` component renders card which displays summary information
+ * about a User's tasks.
+ *
+ * When clicked, navigates to the task details page for the User.
+ * @param {UserTasksCardProps} props - Component properties.
+ * @returns {JSX.Element} JSX
+ */
+const UserTasksCard = ({
+  className,
+  userId,
+  testId = 'card-user-tasks',
+  ...props
+}: UserTasksCardProps): JSX.Element => {
+  const navigate = useNavigate();
+  const { data: tasks, error, isLoading } = useGetUserTasks({ userId });
+  const incompleteTasks = filter(tasks, { completed: false });
+
+  const tasksMessage = useMemo(() => {
+    if (error) {
+      return 'A problem occurred fetching your tasks.';
+    }
+
+    if (incompleteTasks.length === 0) {
+      return 'You are all caught up!';
+    }
+
+    return `You have ${incompleteTasks.length} tasks to complete.`;
+  }, [error, incompleteTasks]);
+
+  return (
+    <div onClick={() => navigate(`/app/users/${userId}/tasks`)} data-testid={testId}>
+      <Card
+        className={classNames(
+          'transition ease-in-out hover:-translate-y-1 hover:scale-105 hover:cursor-pointer hover:bg-blue-600/10',
+          className,
+        )}
+        testId={`${testId}-card`}
+        {...props}
+      >
+        {isLoading ? (
+          <div data-testid={`${testId}-loader`}>
+            <LoaderSkeleton className="mb-2 h-7 w-20" />
+            <LoaderSkeleton className="h-4" />
+          </div>
+        ) : (
+          <div>
+            <div className="text-xl font-bold">Tasks</div>
+            <div data-testid={`${testId}-message`}>{tasksMessage}</div>
+          </div>
+        )}
+      </Card>
+    </div>
+  );
+};
+
+export default UserTasksCard;
