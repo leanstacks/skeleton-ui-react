353d25f825cf8d4a84d0094d4f8c8cdb62d3ed78
Matthew Warman <warmanmr@gmail.com>
2024-05-29 17:38:46 -0400

    #46 tests

--- src/components/Button/__tests__/LanguageToggle.test.tsx
+++ src/components/Button/__tests__/LanguageToggle.test.tsx
@@ -1,9 +1,80 @@
-import { describe, it } from 'vitest';
+import userEvent from '@testing-library/user-event';
+import { describe, expect, it, vi } from 'vitest';
+
+import 'utils/i18n';
+import { StorageKeys } from 'utils/constants';
+import { render, screen } from 'test/test-utils';
+import storage from 'utils/storage';
+
+import LanguageToggle from '../LanguageToggle';
+
+// mock select functions from react-i18next
+const mockChangeLanguage = vi.fn();
+vi.mock('react-i18next', async () => {
+  const original = await vi.importActual('react-i18next');
+  return {
+    ...original,
+    useTranslation: () => ({ i18n: { changeLanguage: mockChangeLanguage } }),
+  };
+});
 
 describe('LanguageToggle', () => {
-  it.skip('should render successfully', async () => {});
+  const setItemSpy = vi.spyOn(storage, 'setItem');
 
-  it.skip('should use custom testId', async () => {});
+  it('should render successfully', async () => {
+    // ARRANGE
+    render(<LanguageToggle />);
+    await screen.findByTestId('dropdown-language');
 
-  it.skip('should use custom className', async () => {});
+    // ASSERT
+    expect(screen.getByTestId('dropdown-language')).toBeDefined();
+  });
+
+  it('should use custom className', async () => {
+    // ARRANGE
+    render(<LanguageToggle className="custom-className" />);
+    await screen.findByTestId('dropdown-language');
+
+    // ASSERT
+    expect(screen.getByTestId('dropdown-language').classList).toContain('custom-className');
+  });
+
+  it('should set language to English', async () => {
+    // ARRANGE
+    render(<LanguageToggle />);
+    await screen.findByTestId('dropdown-language');
+
+    // ACT
+    await userEvent.click(screen.getByTestId('dropdown-item-en'));
+
+    // ASSERT
+    expect(mockChangeLanguage).toHaveBeenCalledWith('en');
+    expect(setItemSpy).toHaveBeenCalledWith(StorageKeys.Language, 'en');
+  });
+
+  it('should set language to French', async () => {
+    // ARRANGE
+    render(<LanguageToggle />);
+    await screen.findByTestId('dropdown-language');
+
+    // ACT
+    await userEvent.click(screen.getByTestId('dropdown-item-fr'));
+
+    // ASSERT
+    expect(mockChangeLanguage).toHaveBeenCalledWith('fr');
+    expect(setItemSpy).toHaveBeenCalledWith(StorageKeys.Language, 'fr');
+  });
+
+  it('should set language to Spanish', async () => {
+    // ARRANGE
+    render(<LanguageToggle />);
+    await screen.findByTestId('dropdown-language');
+
+    // ACT
+    await userEvent.click(screen.getByTestId('dropdown-item-es'));
+
+    // ASSERT
+    expect(mockChangeLanguage).toHaveBeenCalledWith('es');
+    expect(setItemSpy).toHaveBeenCalledWith(StorageKeys.Language, 'es');
+  });
 });
